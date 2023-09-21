508315eb414c978aaa557ad3ce55fff905e2ed32
CDJ <casperdjensen@hotmail.com>
2023-09-21 15:53:44 +0200

    commit

--- src/services/api-config.ts
+++ src/services/api-config.ts
@@ -10,13 +10,3 @@ export const options = {
       Authorization:apiKey,
     },
   };
-
-
-// export default axios.create({
-//     baseURL:'https://api.themoviedb.org/3',
-//     params: {
-//         // accept:'application/json',
-//         // Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMjZiZGM0ZThkZGFmYjEwZGVmYWJiNWY4MjVjYmU3NyIsInN1YiI6IjY1MGMxYzQ0NDRlYTU0MDBlMzIyMjA4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Naa2qU5eMWlWhW82GhalUjDPG7xtrzi3h98-N3fG1ug',
-//         key:'e26bdc4e8ddafb10defabb5f825cbe77',
-//     }
-// })
