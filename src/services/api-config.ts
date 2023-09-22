77650c63251ea00822c865b17dfc54c6597c3c87
CDJ <casperdjensen@hotmail.com>
2023-09-22 12:46:45 +0200

    ff

--- src/services/api-config.ts
+++ src/services/api-config.ts
@@ -1,12 +1,11 @@
 
-const apiKey = import.meta.env.VITE_API_KEY;
-
+const auth = import.meta.env.VITE_AUTH;
 
 export const options = {
     method: "GET",
-    url: "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+    url: `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1`,
     headers: {
       accept: "application/json",
-      Authorization:apiKey,
+      Authorization:auth,
     },
   };
